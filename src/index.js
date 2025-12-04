c58f6184d292cf4535e1fde83cfefc60ddb53715
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-12-04 04:57:30 -0300

    add files. add board graph

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './styles.css';
+
+console.log('SHOWTIME!!!');
+
+let myArray = [];
+let arrayRow = []
+
+for (let x = 0; x <= 7; x++) {
+    arrayRow.push(0);
+};
+
+for (let y = 0; y <= 7; y++) {
+    myArray.push(arrayRow.slice());
+};
+
+console.log(myArray);
+
+function knightMoves(start, end) {
+
+    return `Say hello to 
+multi-line
+strings!`;
+
+
+}
+
+console.log(knightMoves());
